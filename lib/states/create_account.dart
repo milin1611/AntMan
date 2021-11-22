d8acb4138e2b68f21b299538cb992e4d14802f3b
chakaphan <chakaphan00@gmail.com>
2021-11-23 01:07:58 +0700

    update 22/11/2021

--- lib/states/create_account.dart
+++ lib/states/create_account.dart
@@ -1,3 +1,5 @@
+import 'package:antman/utility/my_constant.dart';
+import 'package:antman/widgets/show_title.dart';
 import 'package:flutter/material.dart';
 
 class CreateAccount extends StatefulWidget {
@@ -6,10 +8,62 @@ class CreateAccount extends StatefulWidget {
 }
 
 class _CreateAccountState extends State<CreateAccount> {
+  Row buildName(double size) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: 16),
+          width: size * 0.6,
+          child: TextFormField(
+            decoration: InputDecoration(
+              labelStyle: MyConstant().h3Style(),
+              labelText: 'Name :',
+              prefixIcon: Icon(
+                Icons.fingerprint,
+                color: MyConstant.dark,
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.dark),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: MyConstant.light),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double size = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(title: Text('Create Account'),),
+      appBar: AppBar(
+        title: Text('Create Account'),
+        backgroundColor: MyConstant.primary,
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          buildTitle('ข้อมูลทั่วไป :'),
+          buildName(size),
+          buildTitle('ชนิดของ User :'),
+        ],
+      ),
+    );
+  }
+
+  Container buildTitle(String title) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 16),
+      child: ShowTitle(
+        title: title,
+        textStyle: MyConstant().h2Style(),
+      ),
     );
   }
 }
